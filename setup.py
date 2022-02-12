4c0abcd4a8caf999f0db3a764f91c3b5ad590046
G Johansson <goran.johansson@shiftit.se>
2022-02-12 21:39:42 +0100

    Fix package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 print(find_packages())
 setup(
-    name='pysensibo',
+    name='pysensibov2',
     version='1.0.0',
     description='Python API for Sensibo',
     long_description=long_description,
